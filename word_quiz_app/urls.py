b7f330b257d19da56ec4eca86b4d4464f1978445
Koshikawaxxx1927 <ebi1927@icloud.com>
2023-02-01 12:07:48 +0900

    Delete function is added.

--- word_quiz_app/urls.py
+++ word_quiz_app/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from .views import signupfunc, loginfunc, listfunc, detailfunc
+from .views import signupfunc, loginfunc, listfunc, detailfunc, logoutfunc, edit_quiz, edit_quizsets, delete_quiz
 
 urlpatterns = [
     path('signup/', signupfunc, name='signup'),
     path('login/', loginfunc, name='login'),
     path('list/', listfunc, name='list'),
     path('detail/<int:pk>/', detailfunc, name='detail'),
+    path('logout/', logoutfunc, name='logout'),
+    path('edit_quiz/<int:pk>/', edit_quiz.as_view(), name='edit_quiz'),
+    path('edit_quizsets/', edit_quizsets.as_view(), name='edit_quizsets'),
+    path('delete_quiz/<int:pk>/', delete_quiz.as_view(), name='delete_quiz'),
 ]
--- word_quiz_app/views.py
+++ word_quiz_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from .models import QuizModel, QuizSetModel
+from django.views.generic import ListView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 def signupfunc(request):
     if request.method == 'POST':
@@ -38,3 +41,32 @@ def detailfunc(request, pk):
     quiz_set = get_object_or_404(QuizSetModel, pk=pk, author=request.user)
     quiz = QuizModel.objects.filter(set_name=quiz_set.id, set_name__author=request.user).all()
     return render(request, 'detail.html', {'quiz_set': quiz_set, 'quiz': quiz})
+
+@login_required
+def logoutfunc(request):
+    logout(request)
+    return redirect('login')
+
+class edit_quiz(LoginRequiredMixin, ListView):
+    template_name = 'edit_quiz.html'
+    login_url = 'login'
+    model = QuizSetModel
+    context_object_name = 'quizes'
+    def get(self, request, pk):
+        quiz_set = get_object_or_404(QuizSetModel, pk=pk, author=request.user)
+        quiz = QuizModel.objects.filter(set_name=quiz_set.id, set_name__author=request.user).all()
+        return render(request, self.template_name, {'quiz_set': quiz_set, 'quiz': quiz})
+
+class edit_quizsets(LoginRequiredMixin, ListView):
+    template_name = 'edit_quizsets.html'
+    login_url = 'login'
+    context_object_name = 'quizsets'
+    def get(self, request):
+        queryset = QuizSetModel.objects.filter(author=request.user).all()
+        return render(request, self.template_name, {self.context_object_name : queryset})
+
+class delete_quiz(LoginRequiredMixin, DeleteView):
+    template_name = 'delete_quiz.html'
+    login_url = 'login'
+    model = QuizModel
+    success_url = reverse_lazy('list')
